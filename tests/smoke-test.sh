38422a60bb6cc6962bea71255397e42fd74342ae
Tom Van Herreweghe <tom@theanalogguy.be>
2016-06-23 15:37:47 +0200

    Correctly implement the smoke test
    Signed-off-by: Tom Van Herreweghe <tom@theanalogguy.be>

--- tests/smoke-test.sh
+++ tests/smoke-test.sh
@@ -2,12 +2,12 @@
 
 declare -i timeout=5
 
-while ! TEST_OUTPUT=`curl -s --fail http://localhost`;
+while ! TEST_OUTPUT=`docker run -v $PWD:/tmp exiftool -ver`;
     do sleep 0.1;
 done
 
 ## Assert server response
-if [ "$TEST_OUTPUT" != "Hi there Tom" ]
+if [ "$TEST_OUTPUT" != "10.20" ]
 then
-    echo "Failed asserting that '${TEST_OUTPUT}' equals 'Hi there Tom'" && exit 1;
+    echo "Failed asserting that '${TEST_OUTPUT}' equals '10.20'" && exit 1;
 fi
